132f58de05bf4fc0ff5b98c0eaac193524855dd7
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-05-23 22:27:42 +0530

    Git Auto Push

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "alb-public" {
     source                          = "./vendor/modules/alb"
     ENV                             = var.ENV
+    ALB_NAME                        = roboshop-public
+    INTERNAL                        = false
 }
 
 module "alb-private" {
     source                          = "./vendor/modules/alb"
     ENV                             = var.ENV
+    ALB_NAME                        = roboshop-private
+    INTERNAL                        = true
 }
-
-# We cannot parameterize anything that is added in the source
-
-# To limit that, we can use a tool called as Terrafile. All it does is clones the remote code from the specified branch and make it locally available. 
