fdbb6abb8c6d6e08fbb19a79e8f0aacad1121f37
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-03 15:29:22 +0530

    Git Auto Push

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,11 @@ output "PRIVATE_ALB_ADDRESS" {
 output "PUBLIC_ALB_ADDRESS" {
     value = module.alb-public.ALB_ADDRESS
 }
+
+output "PRIVATE_LISTENER_ARN" {
+    value = module.alb-private.LISTENER_ARN[0]
+}
+
 # output "VPC_ID" {
 #     value = module.vpc.VPC_ID
 # }
